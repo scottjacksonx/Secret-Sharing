be803b6270dab193e8e1bfaf6eeee4e18bf3592a
Scott Jackson <scottjacksonx@gmail.com>
2010-05-18 02:22:33 +1000

    Added ' to the alphabet (for words like "ain't").

--- crypto.py
+++ crypto.py
@@ -1,13 +1,8 @@
 import random
 
-
-
 # Dictionary file from http://wordlist.sourceforge.net/
 
-# Global Variables #
-
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '2', '3', '4', '5', '6', '7', '8', '9', '-', '&', '_', '.', '*', '#']	# '1' is left out for readability's sake (l and 1 can look very similar.)
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '2', '3', '4', '5', '6', '7', '8', '9', '-', '&', '_', '.', '*', '#', '\'']	# '1' is left out for readability's sake ('l' and '1' can look very similar.)
 
 
 # Basic Functions #
@@ -17,7 +12,7 @@ def compareLength(a, b):
 
 def add(l1, l2):
 	"""
-	Adds two letters together and returns the resulting letter (mod 40).
+	Adds two letters together and returns the resulting letter (mod len(alphabet)).
 	"""
 	return alphabet[(alphabet.index(l1) + alphabet.index(l2)) % len(alphabet)]
 	
@@ -39,7 +34,7 @@ def addWords(words):
 	
 def chooseWords(dictionaryFilename, length, numWords=3):
 	"""
-	Looks in a dictionary (text file) and chooses `numWords` random words of length `length`.
+	Looks in a dictionary (text file) and chooses `numWords` random words, at least two of which are length `length` and the rest of length <= `length`.
 	"""
 	dictionary = open(dictionaryFilename, "r")
 	
@@ -50,15 +45,19 @@ def chooseWords(dictionaryFilename, length, numWords=3):
 	
 	randomWords = []
 	random.seed()
-	foundBigWord = False
-	bigRandomWord = ""
-	while not foundBigWord:
-		bigRandomWord = allWords[random.randint(0, len(allWords))]
-		if len(bigRandomWord) == length:
-			foundBigWord = True
-	randomWords.append(bigRandomWord)
 	
-	for i in range(0, numWords - 1):
+	# Get two random words of length `length`.
+	for i in range(0,2):
+		foundBigWord = False
+		bigRandomWord = ""
+		while not foundBigWord:
+			bigRandomWord = allWords[random.randint(0, len(allWords))]
+			if len(bigRandomWord) == length:
+				foundBigWord = True
+		randomWords.append(bigRandomWord)
+	
+	# Get 
+	for i in range(0, numWords - 2):
 		random.seed()
 		appropriatelySizedWordFound = False
 		randomWord = ""
@@ -74,7 +73,7 @@ def chooseWords(dictionaryFilename, length, numWords=3):
 		
 	return randomWords
 
-randomWords = chooseWords("dictionary.txt", 12)
+randomWords = chooseWords("dictionary.txt", 10)
 print randomWords,
 password = addWords(randomWords)
 
